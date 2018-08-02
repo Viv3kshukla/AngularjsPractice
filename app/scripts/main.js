36e264805c93eab151a607e4a86cb2a11ff4e3c1
Viv3kshukla <vivekshukla@iiitdmj.ac.in>
2018-08-02 16:03:53 +0530

    second commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -83,4 +83,29 @@ angular.module('myModule',[]).
       }
 
     });
-  });
+  })
+  .directive('chartDirective',chartDirective);
+
+function chartDirective() {
+  return {
+    restrict: 'A',
+    template: '<div> </div>',
+    scope: {
+      options: '='
+    },
+    link: function (scope , element, attribute) {
+      Highcharts.chart('chart', {
+        chartOptions: {
+          type: 'line'
+        },
+
+        title: {
+          text: 'Temperature data'
+        },
+        series: [{
+          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }]
+      });
+    }
+  }
+}
